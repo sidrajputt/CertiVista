ed62bed4a0a4e04979da26e4be970ccbc814aa8d
sidrajputt <sidrajput037@gmail.com>
2023-11-06 20:04:48 +0530

    updates

--- pdf.js
+++ pdf.js
@@ -51,14 +51,9 @@ document.getElementById('get-doc-btn').addEventListener('click', async () => {
         const lorURL = URL.createObjectURL(lorBlob);
 
         // Create links for downloading the PDFs
-        // const certificateDownloadLink = document.createElement('a');
-        // certificateDownloadLink.href = certificateURL;
-        // certificateDownloadLink.download = name + '' + 'Certificate.pdf';
-        // certificateDownloadLink.textContent = 'Download Custom Certificate';
-        // document.body.appendChild(certificateDownloadLink);
         const certificateDownloadButton = document.createElement('a');
         certificateDownloadButton.href = certificateURL;
-        certificateDownloadButton.download = name + '' + 'Certificate.pdf';
+        certificateDownloadButton.download = name + ' ' + 'Certificate.pdf';
 
         // Apply styles to make it look like a button
         certificateDownloadButton.style.display = 'inline-block'; // Display it as an inline-block element
@@ -70,24 +65,16 @@ document.getElementById('get-doc-btn').addEventListener('click', async () => {
         certificateDownloadButton.style.borderRadius = '5px'; // Add rounded corners
         certificateDownloadButton.style.cursor = 'pointer'; // Change cursor on hover
         certificateDownloadButton.style.marginLeft = '47%'; //
-        // You can also add additional styles as needed
         certificateDownloadButton.style.fontWeight = 'bold'; // Make the text bold
         certificateDownloadButton.style.fontSize = '12px'; // Set the font size
 
         certificateDownloadButton.textContent = 'Download Certificate';
         document.body.appendChild(certificateDownloadButton);
 
-
-        // const lorDownloadLink = document.createElement('a');
-        // lorDownloadLink.href = lorURL;
-        // lorDownloadLink.download = name + '' + 'lor.pdf';
-        // lorDownloadLink.textContent = 'Download Custom LOR';
-        // document.body.appendChild(lorDownloadLink);
         const lorDownloadButton = document.createElement('a');
         lorDownloadButton.href = lorURL;
-        lorDownloadButton.download = name + '' + 'lor.pdf';
+        lorDownloadButton.download = name + ' ' + 'lor.pdf';
 
-        // Apply styles to make it look like a button
         lorDownloadButton.style.display = 'inline-block'; // Display it as an inline-block element
         lorDownloadButton.style.padding = '5px 10px'; // Add padding to create a button-like shape
         lorDownloadButton.style.backgroundColor = '#039be5'; // Background color for the button
@@ -97,7 +84,6 @@ document.getElementById('get-doc-btn').addEventListener('click', async () => {
         lorDownloadButton.style.borderRadius = '5px'; // Add rounded corners
         lorDownloadButton.style.cursor = 'pointer'; // Change cursor on hover
         lorDownloadButton.style.marginLeft = '4px'; // Margin
-        // You can also add additional styles as needed
         lorDownloadButton.style.fontWeight = 'bold'; // Make the text bold
         lorDownloadButton.style.fontSize = '12px'; // Set the font size
 
